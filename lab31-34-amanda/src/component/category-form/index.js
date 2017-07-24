98468373e4e2ab127ef5600de8d2fef4858fa63a
Amanda Koster <amandakoster@gmail.com>
2017-07-24 14:30:44 -0700

    built form

--- lab31-34-amanda/src/component/category-form/index.js
+++ lab31-34-amanda/src/component/category-form/index.js
@@ -11,6 +11,53 @@ import React from 'react'
 class CategoryForm extends React.Component {
   constructor(props){
     super(props)
-    this.state =
+    this.state = {
+      name: props.category ? props.category.name : ''
+      budget: props.budget ? props.category.budget: 0
+
+      this.state = { name, budget }
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handelSubmit.bind(this)
+  }
+
+  handleChange(e){
+    let {name, budget} = e.target
+    this.setState({[name]: budget})
+  }
+
+
+  handelSubmit(e){
+    e.preventDefault()
+    this.props.onComplte(Object.assign({},this.state))
+}
+
+render(){
+  return(
+    <form className='category form' onSubmit={this.handelSubmit}>
+    <input
+    className='title-input'
+    name='title'
+    type='text'
+    placeholder='title'
+    value={this.state.title}
+    onChange={this.handleChange} />
+
+    <input
+    className='price-input'
+    name:'price'
+    type='number'
+    value={this.state.price}
+    onChange={this.handleChange} />
+
+    <button
+    className='button-input'
+    type='submit'>
+    {this.props.buttonName}
+    </button>
+    </form>
+    )
   }
 }
+
+export default CategoryForm
