097ba46f00acbb84f448cb0f34cc3c0f6e0d5216
Scott <sdmccoy@gmail.com>
2017-07-24 15:14:13 -0700

    hello world is up

---
+++ lab31-scott/src/component/app/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+//saves our store and connects all other components with the store
+import {Provider} from 'react-redux';
+import {BrowserRouter, Route} from 'react-router-dom';
+// import DashboardContainer from '../dashboard';
+//import store module that creates our store based on the app state
+// import createAppStore from '../../lib/store.js';
+
+//assing the store to a varible to pass in to the Provider component
+// const store = createAppStore();
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render(){
+    return(
+      <div>
+        Hello World
+      </div>
+    );
+  }
+}
+
+export default App;
