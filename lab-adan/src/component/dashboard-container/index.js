5fe8c989a774b61e6fe97401670c0f3c74daf2a8
adan.rabadan <evilb4k4@gmail.com>
2017-07-25 15:08:00 -0700

    initial commit and stated writing code

---
+++ lab-adan/src/component/dashboard-container/index.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+import {
+  categoryCreate,
+  categoryUpdate,
+  categoryDelete,
+} from '../../action/category-actions.js'
+
+import CategoryForm from '../category-form-component'
+import CategoryItem from '../category-item-component'
+
+class DashboardContainer extends React.Component {
+  render() {
+    return (
+      <main className="dashboard-container">
+        <h2> dashboard </h2>
+        <CategoryForm categoryCreate={this.props.categoryCreate} />
+        <ul id="categories">
+          {this.props.categories.map((item, i) => {
+            return (
+              <li key={i}>
+                <CategoryItem
+                  item={item}
+                  categoryUpdate={this.props.categoryUpdate}
+                  categoryDelete={this.props.categoryDelete}
+                />
+              </li>
+            )
+          })}
+        </ul>
+        ) }
+      </main>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    categories: state,
+  }
+}
+
+const mapDispatchToProps = (dispatch, getState) => {
+  return {
+    categoryCreate: category => dispatch(categoryCreate(category)),
+    categoryUpdate: category => dispatch(categoryUpdate(category)),
+    categoryDelete: category => dispatch(categoryDelete(category)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DashboardContainer)
