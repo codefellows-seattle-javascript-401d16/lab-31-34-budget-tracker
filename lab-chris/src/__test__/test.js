2e058885bb7ac6dc35da3e26d4c81b2e48191970
Chris Janousek <janousek77@gmail.com>
2017-07-25 00:02:31 -0700

    got both create and update passing tests

--- lab-chris/src/__test__/test.js
+++ lab-chris/src/__test__/test.js
@@ -1,10 +1,73 @@
-
 import React from 'react';
 import {shallow} from 'enzyme';
-
+import reducer from '../reducer/category.js';
 
 describe('test', () => {
   test('should pass', () => {
     expect(true).toBe(true);
   });
+});
+
+describe('reducer', () => {
+  test('create', () => {
+    let create = {
+      type: 'CATEGORY_CREATE',
+      payload: {
+        name: 'Computer',
+        budget: 100,
+      },
+    };
+
+    let reduce = reducer(reduce, create);
+    console.log(reduce);
+    expect(reduce[0].name).toEqual('Computer');
+    expect(reduce[0].budget).toEqual(100);
+    expect(reduce[1]).toBeUndefined();
+  });
+
+  test('update', () => {
+    let create = {
+      type: 'CATEGORY_CREATE',
+      payload: {
+        id: 123,
+        name: 'Computer',
+        budget: 100,
+      },
+    };
+
+    let update = {
+      type: 'CATEGORY_UPDATE',
+      payload: {
+        id: 123,
+        name: 'Computer-Mac',
+        budget: 1000,
+      },
+    };
+
+    let reduce = reducer(reduce, create);
+    console.log(create);
+    console.log(create.payload);
+    expect(reduce[0].name).toEqual('Computer');
+    expect(reduce[0].budget).toEqual(100);
+    reduce = reducer(reduce, update);
+    expect(reduce[0].name).toEqual('Computer-Mac');
+    expect(reduce[0].budget).toEqual(1000);
+  });
+
+//   test('delete', () => {
+//     let create = {
+//       type: 'CATEGORY_CREATE',
+//       payload: {
+//         id: 123,
+//         name: 'Computer',
+//         budget: 100,
+//       },
+//     };
+//
+//     let reduce = reducer([], create);
+//     expect(reduce[0].name).toEqual('Computer');
+//     expect(reduce[0].budget).toEqual(100);
+//
+//     reduce = reducer(creat)
+//   });
 });
