f995d683209b40b2d84562d5bb7557bd354e2ccc
Scott <sdmccoy@gmail.com>
2017-07-25 21:58:14 -0700

    built reducers for update and destroy for the expenses

--- lab31-scott/src/reducer/expense.js
+++ lab31-scott/src/reducer/expense.js
@@ -2,6 +2,10 @@ let initialState=[];
 
 export default (state = initialState, action) => {
   let {type, payload} = action;
+  //when an expense is created, get the categoryID from the payload
+  let {categoryID} = payload;
+  //get the array of expenses from the category state
+  let categoryExpenses = state[categoryID];
   switch(type){
   case 'CATEGORY_CREATE':
     //anytime a category is created, spread the state properties, add a property that's the id from the payload, set the value to an empty array. Keep it all in an object
@@ -13,13 +17,19 @@ export default (state = initialState, action) => {
     return {...state, [payload.id]: []};
 
   case 'EXPENSE_CREATE':
-    //when an expense is created, get the categoryID from the payload
-    let {categoryID} = payload;
-    //get the array of expenses from the category state
-    let categoryExpenses = state[categoryID];
     //spread the state object, concat the new payload/expense to the end of the other expenses at the category id property.
     return {...state, [categoryID]: [...categoryExpenses, payload]};
 
+  case 'EXPENSE_UPDATE':
+    return categoryExpenses.map(item => {
+      return item.id === payload.id ? payload : item;
+    });
+
+  case 'EXPENSE_DESTROY':
+    return categoryExpenses.filter(item => {
+      return item.id !== payload.id;
+    });
+    
   default: return state;
   }
 };
