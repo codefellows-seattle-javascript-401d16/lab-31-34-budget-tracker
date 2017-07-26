84c6c773bb28255f829f09096a05bca3ee83a7e9
Oscar Cauich <ocauich@gravitypayments.com>
2017-07-25 23:04:27 -0700

    finished testing the category reducer

--- lab-oscar/src/reducer/category-reducer.test.js
+++ lab-oscar/src/reducer/category-reducer.test.js
@@ -1,89 +1,77 @@
 import categoryReducer from './category.js';
 
+let fakeState, result;
 describe('testing category reducer', () => {
   test('testing jest',() => {
     expect(true).toEqual(true);
   })
+
+  test('it should return an empty Array', () => {
+    result = categoryReducer(undefined, {type: 'whatever'})
+    expect(result).toEqual([])
+  })
+
+  test('if actions if not registered it should return the original state', () => {
+    fakeState = [
+      {id: 1, timestamp: new Date(), Name: 'test', Budget: 100 }
+    ]
+    result = categoryReducer(fakeState, {type: 'testing'});
+    expect(result).toEqual(fakeState);
+  })
+
+  test('CATEGORY_CREATE should append to the state array', () =>  {
+    let actionOne = {
+      type: 'CATEGORY_CREATE',
+      payload: {Name: 'testing', Budget: 100}
+    }
+
+    let fakeState = categoryReducer([], actionOne)
+    expect(fakeState.length).toEqual(1)
+    expect(fakeState[0]).toEqual(actionOne.payload)
+
+    let actionTwo = {
+      type: 'CATEGORY_CREATE',
+      payload: {Name: 'testing-two', Budget: 150}
+    }
+    fakeState = categoryReducer(fakeState, actionTwo)
+    expect(fakeState.length).toEqual(2)
+    expect(fakeState[1]).toEqual(actionTwo.payload)
+    expect(fakeState[0]).toEqual(actionOne.payload)
+  })
+
+  test('CATEGORY_DELETE should remove a category from the array', () => {
+
+    let mockFakeState = [
+      {id: 1, timestamp: new Date(), Name: 'test0', Budget: 100 },
+      {id: 2, timestamp: new Date(), Name: 'test1', Budget: 200 },
+      {id: 3, timestamp: new Date(), Name: 'test2', Budget: 300 },
+      {id: 4, timestamp: new Date(), Name: 'test3', Budget: 400 },
+    ]
+
+    let actionOne = {
+      type: 'CATEGORY_DELETE',
+      payload: mockFakeState[1],
+    }
+    fakeState = categoryReducer(mockFakeState, actionOne)
+    expect(fakeState.length).toEqual(3)
+    expect(fakeState).toEqual(mockFakeState.filter(item => item.id != 2))
+  })
+
+  test('CATEGORY_UPDATE should update a category in the array', () => {
+    let mockFakeState = [
+      {id: 1, timestamp: new Date(), Name: 'test0', Budget: 100 },
+      {id: 2, timestamp: new Date(), Name: 'test1', Budget: 200 },
+      {id: 3, timestamp: new Date(), Name: 'test2', Budget: 300 },
+      {id: 4, timestamp: new Date(), Name: 'test3', Budget: 400 },
+    ]
+    let actionOne = {
+      type: 'CATEGORY_UPDATE',
+      payload: {id: 1, Name: 'testing-update', Budget: 1000 }
+    }
+
+    fakeState = categoryReducer(mockFakeState, actionOne)
+    expect(fakeState[0]).toEqual(actionOne.payload)
+    expect(fakeState.length).toEqual(4)
+    expect(fakeState).toEqual(mockFakeState.map(item => item.id == 1 ? actionOne.payload : item))
+  })
 })
-
-
-// import categoryReducer from '../reducer/category.js'
-//
-// describe('testing category reducer', () => {
-//   test('inital state should be an empty array', () =>{
-//     let result = categoryReducer(undefined, {type: null})
-//     expect(result).toEqual([])
-//   })
-//
-//   test('if the acytion type isnt registerd it will return the state', () =>{
-//     let mockState = [
-//       { id: 'abc',  title: 'cool'},
-//       { id: '123',  title: 'cool'},
-//     ]
-//
-//     let result = categoryReducer(mockState, {type: null})
-//     expect(result).toEqual(mockState)
-//   })
-//
-//   test('CATEGORY_CREATE should append to the array', () => {
-//     let actionOne = {
-//       type: 'CATEGORY_CREATE',
-//       payload: 'hello world',
-//     }
-//
-//     let state = categoryReducer([], actionOne)
-//     expect(state.length).toBe(1)
-//     expect(state[0]).toBe(actionOne.payload)
-//
-//     let actionTwo = {
-//       type: 'CATEGORY_CREATE',
-//       payload: 'goodbye world',
-//     }
-//
-//     state = categoryReducer(state, actionTwo)
-//     expect(state.length).toBe(2)
-//     expect(state[0]).toBe(actionOne.payload)
-//     expect(state[1]).toBe(actionTwo.payload)
-//   })
-//
-//   test('CATEGORY_DELETE should delete a category from the array', () => {
-//     let mockState = [
-//       { id: 'abc',  title: 'cool'},
-//       { id: '123',  title: 'cool'},
-//       { id: 'zyx',  title: 'cool'},
-//       { id: '000',  title: 'cool'},
-//     ]
-//
-//     let actionOne= {
-//       type: 'CATEGORY_DELETE',
-//       payload: mockState[1],
-//     }
-//
-//     let state = categoryReducer(mockState, actionOne)
-//
-//     expect(state.length).toBe(3)
-//     expect(state).toEqual(mockState.filter(item => item.id != '123'))
-//   })
-//
-//   test('CATEGORY_UPDATE should update a category in the array', () => {
-//     let mockState = [
-//       { id: 'abc',  title: 'cool'},
-//       { id: '123',  title: 'cool'},
-//       { id: 'zyx',  title: 'cool'},
-//       { id: '000',  title: 'cool'},
-//     ]
-//
-//     let actionOne= {
-//       type: 'CATEGORY_UPDATE',
-//       payload: {id: 'zyx', title: 'hax'},
-//     }
-//
-//     let state = categoryReducer(mockState, actionOne)
-//
-//     expect(state.length).toBe(4)
-//     expect(state).toEqual(mockState.map(item =>
-//       item.id == 'zyx' ? actionOne.payload : item))
-//   })
-//
-//
-// })
