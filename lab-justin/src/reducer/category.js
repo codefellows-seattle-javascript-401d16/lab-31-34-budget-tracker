77e6babb365bcfd1581112c659118722f0097c48
Justin Green <justin98366@gmail.com>
2017-07-26 12:50:44 -0700

    have some of this weeks assignment created

---
+++ lab-justin/src/reducer/category.js
@@ -0,0 +1,15 @@
+let intialState = [];
+export default (state=intialState, action) => {
+  let {type, payload} = action;
+  switch(type){
+    case 'CATEGORY_CREATE':
+      return [...state, payload];
+    case 'CATEGORY_UPDATE':
+      return state.map(category =>
+        category.id == payload.id ? payload : category);
+    case 'CATEGORY_DESTROY':
+      return state.filter(category => category.id !== payload.id);
+    default:
+      return state;
+  }
+};
