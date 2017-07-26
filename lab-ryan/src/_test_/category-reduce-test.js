4845ea9c5f85196338cde089e239a2508e99d249
Ryan Turner <ryanthepit@gmail.com>
2017-07-25 22:48:11 -0700

    category form is up and running

---
+++ lab-ryan/src/_test_/category-reduce-test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import Reducer from '../reducer';
+
+describe('testing Reducer methods', () => {
+  describe('testing', () => {
+    test('it should pass', () => {
+      
+    });
+  });
+});
