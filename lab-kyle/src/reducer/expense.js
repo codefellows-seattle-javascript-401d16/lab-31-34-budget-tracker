18b1a7c4065ed6a002fac7d7fdcdce896f8d9bc2
Kyle Aardal <aardal.kyle@gmail.com>
2017-07-27 08:42:36 -0700

    added validation

--- lab-kyle/src/reducer/expense.js
+++ lab-kyle/src/reducer/expense.js
@@ -1,21 +1,41 @@
+let validateCategory = category => {
+  if (!category.name || !category.budget || !category.id || !category.timestamp)
+    throw new Error('Category must have a name, budget, id, and timestamp.')
+}
+let validateExpense = expense => {
+  if (
+    !expense.name ||
+    !expense.price ||
+    !expense.id ||
+    !expense.categoryID ||
+    !expense.timestamp
+  )
+    throw new Error(
+      'Expense must have a name, price, id, categoryID, and timestamp'
+    )
+}
+
 export default (state = {}, action) => {
   let { type, payload } = action
-  console.log('expense-reducer payload', payload)
-  console.log('expense-reducer state', state)
+
   switch (type) {
   case 'CATEGORY_CREATE':
+    validateCategory(payload)
     return { ...state, [payload.id]: [] }
 
   case 'CATEGORY_DELETE':
+    validateCategory(payload)
     return { ...state, [payload.id]: undefined }
 
   case 'EXPENSE_CREATE':
+    validateExpense(payload)
     return {
       ...state,
       [payload.categoryID]: [...state[payload.categoryID], payload],
     }
 
   case 'EXPENSE_UPDATE':
+    validateExpense(payload)
     return {
       ...state,
       [payload.categoryID]: state[payload.categoryID].map(
@@ -24,6 +44,7 @@ export default (state = {}, action) => {
     }
 
   case 'EXPENSE_DELETE':
+    validateExpense(payload)
     return {
       ...state,
       [payload.categoryID]: state[payload.categoryID].filter(
