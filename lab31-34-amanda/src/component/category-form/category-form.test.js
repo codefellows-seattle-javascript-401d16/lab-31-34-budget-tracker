b64034fa903997fe286f08984287e6730b89eeca
Amanda Koster <amandakoster@gmail.com>
2017-07-27 07:42:16 -0700

    startedd writing tests

---
+++ lab31-34-amanda/src/component/category-form/category-form.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import CategoryForm from './index.js';
+
+describe ('testing CategoryForm', () => {
+  test('onComplete should be invoked with the state onSubmit' ,() => {
+    let mockHandleOnComplete = jest.fn();
+
+    let wrapper = mount(
+      <CategoryForm onComplete={mockHandleOnComplete} buttonText='submit' />
+    );
+
+    let mockState = {title: 'testing mockstate category form'};
+    wrapper.setState(mockState);
+    wrapper.find('form').simulate('submit');
+
+    let{calls} =
+    mockHandleOnComplete.mock;
+    expect(calls.length).toBe(1);
+    expect(calls[0][0]).toEqual(mockState);
+  });
+
+  test('testing onchnage should update the title on the state', () => {
+    let wrapper = mount(
+      <CategoryForm onComplete={() => {}} buttonText='submit' />
+    );
+
+  });
+});
