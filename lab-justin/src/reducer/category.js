407a88b85bd02995bceba23b25ac22483c56cb0d
Justin Green <justin98366@gmail.com>
2017-07-31 13:23:18 -0700

    lab 32

--- lab-justin/src/reducer/category.js
+++ lab-justin/src/reducer/category.js
@@ -1,15 +1,20 @@
-let intialState = [];
-export default (state=intialState, action) => {
+let initialState = [];
+export default (state=initialState, action) => {
   let {type, payload} = action;
   switch(type){
-    case 'CATEGORY_CREATE':
-      return [...state, payload];
-    case 'CATEGORY_UPDATE':
-      return state.map(category =>
-        category.id == payload.id ? payload : category);
-    case 'CATEGORY_DESTROY':
-      return state.filter(category => category.id !== payload.id);
-    default:
-      return state;
+
+  case 'CATEGORY_CREATE':
+    return [...state, payload];
+
+  case 'CATEGORY_UPDATE':
+    return state.map(category =>
+      category.id == payload.id ? payload : category);
+
+  case 'CATEGORY_DELETE':
+    return state.filter(category =>
+      category.id !== payload.id);
+
+  default:
+    return state;
   }
 };
