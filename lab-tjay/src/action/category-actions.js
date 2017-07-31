f77b7eaf3ad31d7c3b3e7780c855ef580d3937f6
T.Jay <tjayrocket@gmail.com>
2017-07-30 22:49:19 -0700

    Working it as hard as I can :)

--- lab-tjay/src/action/category-actions.js
+++ lab-tjay/src/action/category-actions.js
@@ -8,13 +8,15 @@ export const categoryCreate = (category) => {
     payload: category,
   };
 };
-
 export const categoryUpdate = (category) => ({
   type: 'CATEGORY_UPDATE',
   payload: category,
 });
-
 export const categoryDelete = (category) => ({
   type: 'CATEGORY_DESTROY',
   payload: category,
+});
+export const categoryReset = () =>
+({
+  type: 'CATEGORY_RESET'
 });
