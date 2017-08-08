758e9aab4a1f6bb655955fb51bfc4a62cc987ef0
mikeporter <porter.michael.james@gmail.com>
2017-08-07 18:30:59 -0700

    wrote libs

--- lab-mike/src/component/app/index.js
+++ lab-mike/src/component/app/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {BrowserRouter, Route} from 'react-router-dom';
+import createAppStore from '../../lib/store.js';
 import DashboardContainer from '../dashboard-container';
 
 const store = createAppStore();
